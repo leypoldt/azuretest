3a19ad79d0d461440ea46446c34b955b7ffe17da
unknown <cja@uc-osl-23cy6r1.umoeconsulting.ad>
2013-04-27 14:31:39 +0200

    commit 2

---
+++ MVC4Sample.Web/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC4Sample.Web.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
